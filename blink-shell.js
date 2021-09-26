74b42e8e3da565f852d945e117f512fd0a557d18
Roben Kleene <contact@robenkleene.com>
2021-09-26 12:08:32 -0400

    Tweak colors

--- blink-shell.js
+++ blink-shell.js
@@ -1,4 +1,7 @@
-t.prefs_.set('color-palette-overrides',["#000000", "#b87a7a", "#7ab87a", "#b8b87a", "#7a7ab8", "#b87ab8", "#7ab8b8", "#d9d9d9", "#262626", "#dbbdbd", "#bddbbd", "#dbdbbd", "#bdbddb", "#dbbddb", "#bddbdb", "#ffffff"]);
-	t.prefs_.set('foreground-color', "#d9e6f2");
-	t.prefs_.set('background-color', "#0d1926");
+t.prefs_.set('color-palette-overrides',["#000000", "#FF8E8E", "#75FF95", "#E5E64B", "#87AFDF", "#b879b8", "#4AA4B0", "#d9d9d9", "#252525", "#dabdbd", "#bddabd", "#dadabd", "#0087ff", "#dabdda", "#bddada", "#ffffff"]);
+	t.prefs_.set('foreground-color', "#ffffff");
+	t.prefs_.set('background-color', "#211930");
 	t.prefs_.set('cursor-color', 'rgba(217,230,242,0.5)');
+
+
+
