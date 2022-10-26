81d730e6ebf77a4a1a74f31efe548a97007547b7
Roben Kleene <contact@robenkleene.com>
2022-10-26 09:39:24 -0400

    Fix colors

--- blink-shell/tokyo-night.js
+++ blink-shell/tokyo-night.js
@@ -1,3 +1,3 @@
 t.prefs_.set('color-palette-overrides',["#32344a", "#f7768e", "#9ece6a", "#e0af68", "#7aa2f7", "#ad8ee6", "#449dab", "#787c99", "#444b6a", "#ff7a93", "#b9f27c", "#ff9e64", "#7da6ff", "#bb9af7", "#0db9d7", "#acb0d0"]);
-t.prefs_.set('foreground-color', "#1a1b26");
-t.prefs_.set('background-color', "#a9b1d6");
+t.prefs_.set('foreground-color', "#a9b1d6");
+t.prefs_.set('background-color', "#1a1b26");
