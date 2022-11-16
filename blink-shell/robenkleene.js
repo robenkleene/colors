53f7981c1fcfcad5ea6712d92cdf94ef3b0d2e6f
Roben Kleene <contact@robenkleene.com>
2022-11-15 21:40:56 -0500

    Update colors

--- blink-shell/robenkleene.js
+++ blink-shell/robenkleene.js
@@ -1,4 +1,4 @@
-t.prefs_.set('color-palette-overrides',["#000000", "#9D571A", "#75FF95", "#E5E64B", "#87afd7", "#C978EB", "#00afaf", "#d9d9d9", "#252525", "#dabdbd", "#bddabd", "#dadabd", "#3792E3", "#DBA3EB", "#00ffff", "#ffffff"]);
+t.prefs_.set('color-palette-overrides',["#000000", "#9D571A", "#75FF95", "#E5E64B", "#87AFDF", "#C978EB", "#00afaf", "#FFFFFF", "#999999", "#dabdbd", "#bddabd", "#dadabd", "#3792E3", "#DBA3EB", "#00ffff", "#999999"]);
 t.prefs_.set('foreground-color', "#ffffff");
 t.prefs_.set('background-color', "#211930");
 t.prefs_.set('cursor-color', 'rgba(69,150,131,0.5)');
